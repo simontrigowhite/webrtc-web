697adfb2276da220785b5de37f9956a9a8fc7696
Simon White <simontrigowhite@gmail.com>
2021-01-23 20:55:56 +0000

    switch on audio

--- work/js/main.js
+++ work/js/main.js
@@ -5,11 +5,13 @@
 // In this codelab, you will be streaming video only: "video: true".
 // Audio will not be streamed because it is set to "audio: false" by default.
 const mediaStreamConstraints = {
+  audio: true,
   video: true,
 };
 
 // Set up to exchange only video.
 const offerOptions = {
+  offerToReceiveAudio: 1,
   offerToReceiveVideo: 1,
 };
 
