ac4a63ced7b7cd00d1dd65f1172e643c32d8062c
Simon White <simontrigowhite@gmail.com>
2021-01-30 01:10:39 +0000

    get on server with https

--- work/index.js
+++ work/index.js
@@ -11,6 +11,9 @@ var app = http.createServer(function(req, res) {
 }).listen(8080);
 
 var io = socketIO.listen(app);
+
+//http.listen(8080);
+
 io.sockets.on('connection', function(socket) {
 
   // convenience function to log server messages on the client
